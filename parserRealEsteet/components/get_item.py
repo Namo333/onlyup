ba8aa9186d67491ff26d8a26420132413050d75c
Namo333 <dad688634@gmail.com>
2024-02-12 11:26:30 +0300

    2

--- parserRealEsteet/components/get_item.py
+++ parserRealEsteet/components/get_item.py
@@ -1,2 +1,13 @@
-def getItem():
-    pass
+class getItem:
+    @staticmethod
+    def nameHome(soup):
+        homeNameAddres = soup.find_all('h3', class_='object-block-title')
+        data_list = []
+        for h3 in homeNameAddres:
+            nameLinks = h3.find_all('a')
+            for nameAndHref in nameLinks:
+                dataName = nameAndHref.text
+                dataHref = nameAndHref.get('href')
+                data = f"{dataHref} - {dataName}"
+                data_list.append(data)
+        return data_list
