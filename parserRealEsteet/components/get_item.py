097c5c6c3dcfa015228c2c0941c4d6c59d0bd969
Namo333 <dad688634@gmail.com>
2024-02-13 13:03:04 +0300

    add pars img and create json

--- parserRealEsteet/components/get_item.py
+++ parserRealEsteet/components/get_item.py
@@ -1,13 +1,49 @@
 class getItem:
     @staticmethod
-    def nameHome(soup):
-        homeNameAddres = soup.find_all('h3', class_='object-block-title')
+    def getName(soup):
+        homeName = soup.find_all('h3', class_='object-block-title')
         data_list = []
-        for h3 in homeNameAddres:
+        for h3 in homeName:
             nameLinks = h3.find_all('a')
             for nameAndHref in nameLinks:
                 dataName = nameAndHref.text
-                dataHref = nameAndHref.get('href')
-                data = f"{dataHref} - {dataName}"
+                data = {'nameEstete': dataName}
                 data_list.append(data)
         return data_list
+    
+    @staticmethod
+    def getAddres(soup):
+        pass
+
+    @staticmethod
+    def getDeveloper(soup):
+        pass
+
+    @staticmethod
+    def getRooms(soup):
+        pass
+
+    @staticmethod
+    def getSquare(soup):
+        pass
+
+    @staticmethod
+    def getPrice(soup):
+        pass
+
+    @staticmethod
+    def getImage(soup):
+        image_list = []
+        blockCont = soup.find_all('div', class_='object-block-holder')
+        for aImg in blockCont:
+            findImg = aImg.find_all('a', class_='object-block-image')
+            for img in findImg:
+                imgSrc = img.find_all('img')
+                for ansImg in imgSrc:
+                    image_url = ansImg.get('src')
+                    image_data = {
+                        'image_url': image_url
+                    }
+                    image_list.append(image_data)
+        return image_list
+
--- parserRealEsteet/main.py
+++ parserRealEsteet/main.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 from components.get_url import getUrl
 from components.get_item import getItem
@@ -11,11 +12,30 @@ proxy = {
 
 def main():
     soup = getUrl(url, proxy)
-    nameRealEst = getItem.nameHome(soup)
+    nameRealEst = getItem.getName(soup)
+    addresRealEst = getItem.getAddres(soup)
+    developerRealEst = getItem.getDeveloper(soup)
+    roomsRealEst = getItem.getRooms(soup)
+    squareRealEst = getItem.getSquare(soup)
+    priceRealEst = getItem.getPrice(soup)
+    imageRealEst = getItem.getImage(soup)
 
-    for data in nameRealEst:
-        print(data)
+    data_list = []
+    for idx, data in enumerate(nameRealEst, start=1):
+        data_dict = {
+            "id": idx,
+            "nameEstete": data['nameEstete'],
+            "addresEstete": addresRealEst[idx - 1]['addresEstete'] if addresRealEst and idx <= len(addresRealEst) else None,
+            "developerEstete": developerRealEst[idx - 1]['developerEstete'] if developerRealEst and idx <= len(developerRealEst) else None,
+            "roomsEstete": roomsRealEst[idx - 1]['roomsEstete'] if roomsRealEst and idx <= len(roomsRealEst) else None,
+            "squareEstete": squareRealEst[idx - 1]['squareEstete'] if squareRealEst and idx <= len(squareRealEst) else None,
+            "priceEstete": priceRealEst[idx - 1]['priceEstete'] if priceRealEst and idx <= len(priceRealEst) else None,
+            "image_url": imageRealEst[idx - 1]['image_url'] if imageRealEst and idx <= len(imageRealEst) else None
+        }
+        data_list.append(data_dict)
+
+    with open('data.json', 'w', encoding='utf-8') as file:
+        json.dump(data_list, file, ensure_ascii=False, indent=4)
 
 if __name__ == "__main__":
     main()
-    
